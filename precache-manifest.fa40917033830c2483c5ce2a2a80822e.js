486b9c2ab71a3321fa69bec65cc7cb31122178d6
Youssef Shaaban <youssef@gmail.com>
2020-07-15 02:41:41 +0200

    Updates

---
+++ precache-manifest.fa40917033830c2483c5ce2a2a80822e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619ab9fa45a6cd64475428f7bbf8afbc",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f72f3ba3633b7851c50d",
+    "url": "/test/static/css/main.ee536b3d.chunk.css"
+  },
+  {
+    "revision": "7a30347523a60f3ef773",
+    "url": "/test/static/js/2.d427c2e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.d427c2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72f3ba3633b7851c50d",
+    "url": "/test/static/js/main.a989b6f1.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
